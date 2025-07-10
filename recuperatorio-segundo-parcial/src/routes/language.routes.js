5855f3968c015cb0ed4986567c4a114eb2d58417
Laura Acosta <acostamarialauraa@gmail.com>
2025-07-10 15:24:30 -0300

    Se realizaron las bases de las rutas

---
+++ recuperatorio-segundo-parcial/src/routes/language.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+//import (peticiones)
+
+export const languagesRouter = express.Router()
+
+
+languagesRouter.get ("/languages",)
+languagesRouter.get ("/languages",)
+languagesRouter.put ("/languages",)
+languagesRouter.post ("/languages",)
+languagesRouter.delete ("/languages",)

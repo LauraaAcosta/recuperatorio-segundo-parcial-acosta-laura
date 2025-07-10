f36e09be71bd66a67f7977b1ac35c46d760affb1
Laura Acosta <acostamarialauraa@gmail.com>
2025-07-10 15:19:21 -0300

    Se realizaron modificaciones en database y el modelo de lenguaje

--- recuperatorio-segundo-parcial/src/config/database.js
+++ recuperatorio-segundo-parcial/src/config/database.js
@@ -8,7 +8,8 @@ export const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        dialect: process.env.DB_HOST || "localhost"
+        dialect: process.env.DB_DIALECT || "mysql",
+        host: process.env.DB_HOST || "localhost"
     }
 );
 
---
+++ recuperatorio-segundo-parcial/src/models/language.model.js
@@ -0,0 +1,25 @@
+import { sequelize  } from "../config/database.js";
+import { DataTypes, STRING } from "sequelize";
+
+export const languages = sequelize.define( "Languages",
+    {
+        id: {
+            type:DataTypes.INTEGER(),
+            autoIncrement: true,
+            primaryKey: true, 
+        },
+        name: {
+            type:DataTypes.STRING(),
+            allowNull:false,
+            unique: true,
+        },
+        paradigm: {
+            type:DataTypes.STRING(),
+            allowNull:false,
+        },
+        release_date: {
+            type:DataTypes.INTEGER(),
+            allowNull:true,
+        },
+    }
+);
